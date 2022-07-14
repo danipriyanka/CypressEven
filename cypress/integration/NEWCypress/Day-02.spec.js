12253b402c172ebccdf0095d3f2e9819411dddb0
danipriyanka1998 <danipriyanka1998@gmail.com>
2022-07-14 12:59:33 +0530

    Trav

--- cypress/integration/NEWCypress/Day-02.spec.js
+++ cypress/integration/NEWCypress/Day-02.spec.js
@@ -2,16 +2,16 @@
 describe('TO learn Traversing methods',()=>{
     //children(),first(),last()
     //siblings-----next(),prev()
-    it.only('TC-01 To get children of DOM elements, use the .children() command.',()=>{
+    it('TC-01 To get children of DOM elements, use the .children() command.',()=>{
         cy.visit('https://www.kesari.in/')
         cy.get('ul[class="menu list-unstyled mb-0"]').children().should('have.length',11)
         cy.get('ul[class="menu list-unstyled mb-0"]').children().should('have.length.above',8)
     })
-    it.skip('TC-02 To get the first DOM element within elements, use the .first() command.',()=>{
+    it('TC-02 To get the first DOM element within elements, use the .first() command.',()=>{
         cy.visit('https://www.kesari.in/')
         cy.get('ul[class="menu list-unstyled mb-0"]').children().first().should('contain','Group')
     })
-    it.skip('TC-03 To get the first DOM element within elements, use the .last() command.',()=>{
+    it('TC-03 To get the first DOM element within elements, use the .last() command.',()=>{
         cy.visit('https://www.kesari.in/')
         cy.get('ul[class="menu list-unstyled mb-0"]').children().last().should('contain','More')
     })
